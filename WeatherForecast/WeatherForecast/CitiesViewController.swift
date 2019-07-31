e71c940105f2d3340e33671eedd1a524a1452261
junyng <pom0894@naver.com>
2019-07-31 18:18:33 +0900

    CollectionView 추가 및 UICollectionViewDelegate 준수

--- WeatherForecast/WeatherForecast/CitiesViewController.swift
+++ WeatherForecast/WeatherForecast/CitiesViewController.swift
@@ -9,8 +9,27 @@
 import UIKit
 
 class CitiesViewController: UIViewController {
-
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
+
+// MARK: UICollectionViewDataSource
+extension CitiesViewController: UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "city", for: indexPath)
+        return cell
     }
 }
