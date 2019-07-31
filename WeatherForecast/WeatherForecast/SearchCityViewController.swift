64dcd4053b17f6b1362c5df57ea378c6370818af
junyng <pom0894@naver.com>
2019-07-31 21:11:51 +0900

    TableView 추가 및 UITableViewDataSource 준수

--- WeatherForecast/WeatherForecast/SearchCityViewController.swift
+++ WeatherForecast/WeatherForecast/SearchCityViewController.swift
@@ -9,8 +9,23 @@
 import UIKit
 
 class SearchCityViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
+
+// MARK: UITableViewDataSource
+extension SearchCityViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "UITableViewCell")
+        cell.textLabel?.text = "도시"
+        return cell
     }
 }
