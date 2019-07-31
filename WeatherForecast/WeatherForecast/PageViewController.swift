86f80ac65bb584277c53a697946f52ecd036aa1f
junyng <pom0894@naver.com>
2019-08-01 01:01:12 +0900

    PageViewControllerDataSource 및 PageViewControllerDelegate 프로토콜 준수, ViewController 배열 정의 후 PageViewController의 첫번째 뷰 컨트롤러 설정

--- WeatherForecast/WeatherForecast/PageViewController.swift
+++ WeatherForecast/WeatherForecast/PageViewController.swift
@@ -9,8 +9,44 @@
 import UIKit
 
 class PageViewController: UIPageViewController {
-
+    
+    fileprivate lazy var pages: [UIViewController] = {
+        return [
+            UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "weatherViewController"),
+            UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "weatherViewController")
+        ]
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.dataSource = self
+        self.delegate   = self
+        if let firstViewController = pages.first
+        {
+            setViewControllers([firstViewController], direction: .forward, animated: true, completion: nil)
+        }
     }
 }
+
+// MARK: UIPageViewControllerDataSource
+extension PageViewController: UIPageViewControllerDataSource {
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = pages.firstIndex(of: viewController) else { return nil }
+        let previousIndex = viewControllerIndex - 1
+        guard previousIndex >= 0 else { return pages.last }
+        guard pages.count > previousIndex else { return nil }
+        return pages[previousIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController?
+    {
+        guard let viewControllerIndex = pages.firstIndex(of: viewController) else { return nil }
+        let nextIndex = viewControllerIndex + 1
+        guard nextIndex < pages.count else { return pages.first }
+        guard pages.count > nextIndex else { return nil }
+        return pages[nextIndex]
+    }
+}
+
+// MARK: UIPageViewControllerDelegate
+extension PageViewController: UIPageViewControllerDelegate { }
