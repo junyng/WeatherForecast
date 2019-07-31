64cb76dfb9bef17ba5bcaf8c1858af8b429d9b5b
junyng <pom0894@naver.com>
2019-07-31 20:54:26 +0900

    도시를 검색하는 SearchCityViewController 클래스 및 파일 추가

---
+++ WeatherForecast/WeatherForecast/SearchCityViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SearchCityViewController.swift
+//  WeatherForecast
+//
+//  Created by BLU on 31/07/2019.
+//  Copyright © 2019 BLU. All rights reserved.
+//
+
+import UIKit
+
+class SearchCityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
