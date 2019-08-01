9562415a1dd146adf6b534b442b687044fc43582
junyng <pom0894@naver.com>
2019-08-02 04:43:23 +0900

    날씨 데이터 모델 추가 Weather - 전체 날씨 정보, Currently -현재 시간의 날씨 정보

---
+++ WeatherForecast/WeatherForecast/Model/Weather.swift
@@ -0,0 +1,14 @@
+//
+//  Weather.swift
+//  WeatherForecast
+//
+//  Created by BLU on 02/08/2019.
+//  Copyright © 2019 BLU. All rights reserved.
+//
+
+import Foundation
+
+/// 전체 날씨 정보
+struct Weather: Codable {
+    let currently: Currently
+}
