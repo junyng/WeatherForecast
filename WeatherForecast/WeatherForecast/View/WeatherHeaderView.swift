77aab18fbce34547a9b0fdb8c64904dceed097c2
junyng <pom0894@naver.com>
2019-08-03 22:34:31 +0900

    WeatherHeaderView 분리 및 클래스 추가

---
+++ WeatherForecast/WeatherForecast/View/WeatherHeaderView.swift
@@ -0,0 +1,30 @@
+//
+//  WeatherHeaderView.swift
+//  WeatherForecast
+//
+//  Created by BLU on 03/08/2019.
+//  Copyright © 2019 BLU. All rights reserved.
+//
+
+import UIKit
+
+class WeatherHeaderView: UICollectionReusableView {
+    @IBOutlet weak var collectionView: UICollectionView! {
+        didSet {
+            collectionView.dataSource = self
+        }
+    }
+    
+}
+
+extension WeatherHeaderView: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "hourly", for: indexPath)
+        cell.backgroundColor = .blue
+        return cell
+    }
+}
