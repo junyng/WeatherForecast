0449e82ce3d0e23f3f265bf972130f075b8e2f60
junyng <pom0894@naver.com>
2019-08-03 23:04:00 +0900

    날씨의 전반적인 정보를 나타내는 SummaryCell 클래스 추가

---
+++ WeatherForecast/WeatherForecast/View/SummaryCell.swift
@@ -0,0 +1,13 @@
+//
+//  SummaryCell.swift
+//  WeatherForecast
+//
+//  Created by BLU on 03/08/2019.
+//  Copyright © 2019 BLU. All rights reserved.
+//
+
+import UIKit
+
+class SummaryCell: UICollectionViewCell {
+    @IBOutlet weak var summaryTextView: UITextView!
+}
