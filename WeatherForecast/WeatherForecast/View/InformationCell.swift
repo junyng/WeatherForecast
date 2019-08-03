e78ab3646ea9005486ca74fcd515cd78a352d545
junyng <pom0894@naver.com>
2019-08-04 01:55:47 +0900

    날씨 정보를 나타내는 InformationCell 추가

---
+++ WeatherForecast/WeatherForecast/View/InformationCell.swift
@@ -0,0 +1,14 @@
+//
+//  InformationCell.swift
+//  WeatherForecast
+//
+//  Created by BLU on 03/08/2019.
+//  Copyright © 2019 BLU. All rights reserved.
+//
+
+import UIKit
+
+class InformationCell: UICollectionViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var contentLabel: UILabel!
+}
