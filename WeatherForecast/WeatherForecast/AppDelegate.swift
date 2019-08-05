5c9afd7f69df403a68695dff70e0b96a6d7d84dc
junyng <pom0894@naver.com>
2019-08-05 12:34:27 +0900

    백그라운드로 진입 할 시, CoordinateStore의 Coordinates 배열을 저장하도록 적용

--- WeatherForecast/WeatherForecast/AppDelegate.swift
+++ WeatherForecast/WeatherForecast/AppDelegate.swift
@@ -12,14 +12,17 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    let coordinateStore = CoordinateStore.sharedInstance
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        let coordinateStore = CoordinateStore.sharedInstance
         let navigationController = window!.rootViewController as! UINavigationController
         let locationTableViewController = navigationController.topViewController as! LocationTableViewController
         locationTableViewController.coordinateStore = coordinateStore
         return true
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        coordinateStore.saveData()
     }
 }
 
