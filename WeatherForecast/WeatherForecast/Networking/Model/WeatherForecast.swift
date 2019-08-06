22a6f08cf312ad963666bea2e311475d6afb245b
junyng <pom0894@naver.com>
2019-08-07 01:30:37 +0900

    불필요한 코드 제거, 모델 경로 변경

--- WeatherForecast/WeatherForecast/Networking/Model/WeatherForecast.swift
+++ WeatherForecast/WeatherForecast/Networking/Model/WeatherForecast.swift
@@ -21,31 +21,3 @@ struct WeatherForecast: Codable {
     let weatherHourly: WeatherHourly
     let weatherDaily: WeatherDaily
 }
-
-extension WeatherForecast {
-    static func fetchWeather(
-        coordinate: (Double, Double),
-        dispatcher: NetworkDispatcher = NetworkDispatcher.sharedInstance,
-        completion: @escaping (Result<WeatherForecast, Error>) -> Void
-        ) {
-        let darkSkyRequest = DarkSkyRequest(coordinate: coordinate)
-        dispatcher.dispatch(request: darkSkyRequest) { (result) in
-            switch result {
-            case .success(let data):
-                do {
-                    let jsonDecoder = JSONDecoder()
-                    let result = try jsonDecoder.decode(WeatherForecast.self, from: data)
-                    DispatchQueue.main.async {
-                        completion(.success(result))
-                    }
-                } catch {
-                    DispatchQueue.main.async {
-                        completion(.failure(error))
-                    }
-                }
-            case .failure(let error):
-                completion(.failure(error))
-            }
-        }
-    }
-}
